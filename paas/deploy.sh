6c151a7f639bd000919d60619762d5b748cd1348
marcin-rp <mratajczak@man.poznan.pl>
2024-03-04 09:51:08 +0100

    MET-5812 Deployed Flink client

--- paas/deploy.sh
+++ paas/deploy.sh
@@ -6,6 +6,9 @@ echo Checking project...
 oc project | grep ecloud-flink-poc
 echo Project OK
 
+#Configure volumes
+oc apply -f deployments-volume.yaml
+
 # Configuration and service definition
 oc apply -f flink-configuration-configmap.yaml
 oc apply -f jobmanager-service.yaml
@@ -13,3 +16,6 @@ oc apply -f jobmanager-service.yaml
 # Create the deployments for the cluster
 oc apply -f jobmanager-session-deployment-non-ha.yaml
 oc apply -f taskmanager-session-deployment.yaml
+
+# Create client
+oc apply -f flink-client.yaml
