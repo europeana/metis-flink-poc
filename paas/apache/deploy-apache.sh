06d8914123c9ed6a5664e3fcebe09e929fc084fc
marcin-rp <mratajczak@man.poznan.pl>
2024-03-05 13:15:55 +0100

    MET-5812 Simplified apache deploying script.

--- paas/apache/deploy-apache.sh
+++ paas/apache/deploy-apache.sh
@@ -4,6 +4,12 @@ echo Checking project...
 oc project | grep ecloud-flink-poc
 echo Project OK
 
+#oc create secret generic htpasswd --from-file=config/apache/.htpasswd
+oc delete configmap apache-dashboard
+oc create configmap apache-dashboard --from-file=dashboard.conf
+oc delete configmap apache-mod-security
+oc create configmap apache-mod-security --from-file=mod_security.conf
+
 oc apply -f apache.yaml
 oc apply -f apache-service.yaml
 oc apply -f apache-route.yaml
