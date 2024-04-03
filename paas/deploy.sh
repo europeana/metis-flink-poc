6d0db2e919b49476d36d2a2eccbc43547904f2de
marcin-rp <mratajczak@man.poznan.pl>
2024-04-03 20:49:27 +0200

    MET-5876 Task manager pod on the openshift now uses private (non-persistent volume) for dump storing.

--- paas/deploy.sh
+++ paas/deploy.sh
@@ -11,7 +11,6 @@ oc apply -f deployments-volume.yaml
 oc apply -f web-upload-volume.yaml
 oc apply -f job-manager-working-volume.yaml
 oc apply -f job-manager-dumps-volume.yaml
-oc apply -f task-manager-dumps-volume.yaml
 # Configuration and service definition
 oc apply -f flink-configuration-configmap.yaml
 oc apply -f jobmanager-service.yaml
