cc933431737f84dfc17b3091169153421f59fa02
marcin-rp <mratajczak@man.poznan.pl>
2024-07-04 15:36:56 +0200

    MET-5995 Migrated JobExecutor from RestClient to RestTemplate to be compatible with spring web 5.

--- flink-poc-client/src/main/java/eu/europeana/cloud/flink/client/JobExecutor.java
+++ flink-poc-client/src/main/java/eu/europeana/cloud/flink/client/JobExecutor.java
@@ -3,22 +3,23 @@ package eu.europeana.cloud.flink.client;
 import eu.europeana.cloud.flink.client.entities.JobDetails;
 import eu.europeana.cloud.flink.client.entities.SubmitJobRequest;
 import eu.europeana.cloud.flink.client.entities.SubmitJobResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientResponseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
 
 public class JobExecutor {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(JobExecutor.class);
 
-  private final String authHeader;
   private final String jarId;
-  private final RestClient restClient;
+  private final RestTemplate restTemplate;
+  private final String serverUrl;
+  private final HttpHeaders httpHeader;
 
   public JobExecutor(Properties serverConfiguration) {
     this(serverConfiguration.getProperty("job.manager.url"),
@@ -35,11 +36,10 @@ public class JobExecutor {
   }
 
   public JobExecutor(String serverUrl, String user, String password, String jarId) {
-    authHeader = "Basic " + Base64.getEncoder().encodeToString((user + ":" + password).getBytes(StandardCharsets.UTF_8));
-    restClient = RestClient.builder()
-                           .baseUrl(serverUrl)
-                           .defaultHeader("Authorization", authHeader)
-                           .build();
+    this.serverUrl = serverUrl;
+    httpHeader = new HttpHeaders();
+    httpHeader.setBasicAuth(user, password);
+    restTemplate = new RestTemplate();
     this.jarId = jarId;
   }
 
@@ -47,39 +47,28 @@ public class JobExecutor {
     String jobId = submitJob(request);
 
     JobDetails details;
-    int i=0;
+    int i = 0;
     do {
       Thread.sleep(200L);
       details = getProgress(jobId);
-      if(++i%5==0){
+      if (++i % 5 == 0) {
         LOGGER.info("Progress: {}", details);
       }
-    } while (!details.getState().equals("FINISHED"));
+    } while (!(details.getState().equals("FINISHED") || details.getState().equals("CANCELED")));
     System.out.println("");
     LOGGER.info("Job finished! Details: {}", details);
   }
 
   public JobDetails getProgress(String jobId) {
-    return restClient.get()
-                     .uri("jobs/" + jobId)
-                     .retrieve()
-                     .body(JobDetails.class);
+    return restTemplate.exchange(serverUrl+"/jobs/" + jobId, HttpMethod.GET, new HttpEntity(httpHeader), JobDetails.class).getBody();
   }
 
   private String submitJob(SubmitJobRequest request) {
-    try {
-      SubmitJobResponse result =
-          restClient.post()
-                    .uri("jars/" + jarId + "/run?entry-class=" + request.getEntryClass())
-                    .body(request)
-                    .retrieve()
-                    .body(SubmitJobResponse.class);
-      LOGGER.info("Submitted Job: {} Submission result:\n{}\nExecuting...", request, result);
-      return result.getJobid();
-    } catch (RestClientResponseException e) {
-      String message = e.getMessage().replaceAll("\\\\n\\\\t", "\n");
-      throw new RuntimeException("Error submitting job! " + message, e);
-    }
+    SubmitJobResponse result = restTemplate.exchange(
+        serverUrl + "/jars/" + jarId + "/run?entry-class=" + request.getEntryClass()
+        , HttpMethod.POST, new HttpEntity<>(request, httpHeader), SubmitJobResponse.class).getBody();
+    LOGGER.info("Submitted Job: {} Submission result:\n{}\nExecuting...", request, result);
+    return result.getJobid();
   }
 
 }
